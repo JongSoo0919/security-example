5f06fc97555b1150f2312cddccdd3a85cc3f00dc
jskim5 <prjs3643@gmail.com>
2023-10-26 16:34:39 +0900

    Revert "토큰을 통한 로그인 구현"
    This reverts commit e66aa7b9c768014279ea29e03dbfaa6025d4de46.

--- src/main/java/com/exam/security/config/SecurityConfig.java
+++ src/main/java/com/exam/security/config/SecurityConfig.java
@@ -1,20 +1,16 @@
 package com.exam.security.config;
 
 import jakarta.servlet.DispatcherType;
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
